2105b68a31c939ec167b24a2f47a0dac0a495aa8
Olle E. Johansson <oej@edvina.net>
2024-09-10 17:52:04 +0200

    Adding some initial init functions

--- tea-collection/__init__.py
+++ tea-collection/__init__.py
@@ -10,16 +10,51 @@ class collection:
     debug = False
     name = None
     collection = None
+    uuid = None
 
     def __init__(self, debug):
         """Initialise collection object"""
         self.debug = debug
+        self.generate_uuid()
+
 
     def __str__(self):
         """Return a printable dnsobject in json."""
         import json
         printobj = dict(self.dnsobject)
         return json.dumps(printobj, sort_keys=False, indent=4)
+
+    def generate_uuid(self):
+        """Return an UUID v 4."""
+        import uuid
+
+        if self.uuid != None:
+            if self.debug:
+                print("DEBUG: Error - Attempting to re-initialise collection structure.\n")
+            return False
+        self.uuid = uuid.uuid4()
+        if self.debug:
+            print("DEBUG: Generated new UUID: {}".format(str(self.uuid)))
+        return True
+
+    def init_struct(self):
+        """Initialise empty structure."""
+        if self.collection != None:
+            if self.debug:
+                print("DEBUG: Error - Attempting to re-initialise collection structure.\n")
+            return False
+        collection = dict()
+        collection["UUID"] = str(self.uuid)
+        collection["product_name"] = None
+        collection["product_version"] = None
+        collection["product_release_date"] = None
+        collection["version"] = 0
+        collection["author_name"] = None
+        collection["author_org"] = None
+        collection["author_email"] = None
+        collection["artefacts"] = list()
+
+        self.collection = collection
 
 
 class artefact:
@@ -35,3 +70,35 @@ class artefact:
         import json
         printobj = dict(self.dnsobject)
         return json.dumps(printobj, sort_keys=False, indent=4)
+    
+    def init_struct(self):
+        import uuid
+        is self.artefact != None:
+            if self.debug:
+                    print("DEBUG: Error - Attempting to re-initialise artefact structure.\n")
+            return False
+        artefact = dict()
+        artefact["uuid"] = str(uuid.uuid4())
+        artefact["name"] = None
+        artefact["description"] = None
+        artefact["author_name"] = None
+        artefact["author_org"] = None
+        artefact["author_email"] = None
+        artefact["formats"] = list()
+        self.artefact = artefact
+        return artefact
+
+    def init_format(self):
+        import uuid
+        format = dict()
+        format["uuid"] = str(uuid.uuid4())
+        format["bom-identifier"] = None
+        format["mediatype"] = None
+        format["category"] = None
+        format["url"] = None
+        format["sigurl"] = None
+        format["hash"] = None
+        format["size"] = 0
+        return format
+
+
